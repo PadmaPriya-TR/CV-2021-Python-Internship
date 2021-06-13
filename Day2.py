56631ebff2ea465d4cfb25d4203e0f52f8590a78
PadmaPriya-TR <85836583+PadmaPriya-TR@users.noreply.github.com>
2021-06-13 21:36:25 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,57 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("30 days 30 hour challenge")
+30 days 30 hour challenge
+>>> print('30 days 30 hour challenge')
+30 days 30 hour challenge
+>>> Hours="thirty"
+>>> print(Hours)
+thirty
+>>> Days="Thirty days"
+>>> print(Days[0])
+T
+>>> print(Days[5])
+y
+>>> Challenge="I will win"
+>>> print(len(Challenge))
+10
+>>> print(Challenge.lower())
+i will win
+>>> print(Challenge.upper())
+I WILL WIN
+>>> print(Challenge[7:10])
+win
+>>> a="30 days"
+>>> b="30 hours"
+>>> c=a+b
+>>> print(c)
+30 days30 hours
+>>> text="Thirty days and Thirty hours"
+>>> x=text.casefold()
+>>> print(x)
+thirty days and thirty hours
+>>> x=text.capitalize()
+>>> print(x)
+Thirty days and thirty hours
+>>> x=text.isalpha()
+>>> print(x)
+False
+>>> x=text.isalnum()
+>>> print(x)
+False
+>>> print(text.title())
+Thirty Days And Thirty Hours
+>>> print(text.swapcase())
+tHIRTY DAYS AND tHIRTY HOURS
+>>> x=text.find("hours")
+>>> print(x)
+23
+>>> print(text.find("hate"))
+-1
+>>> word="Internship"
+>>> print(word.isalpha())
+True
+>>> word="Internship1"
+>>> print(word.isalnum())
+True
+>>> 
